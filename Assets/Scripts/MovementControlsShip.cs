e011597564b725b953220d0a33ff280646b6ecf2
Samuel Jones <samuel.jones@live.com.au>
2019-06-25 17:31:48 +1000

    Ship movement controls: Unrefined

--- Assets/Scripts/MovementControlsShip.cs
+++ Assets/Scripts/MovementControlsShip.cs
@@ -7,28 +7,65 @@ public class MovementControlsShip : MonoBehaviour
 {
     //VARIABLES
     private Rigidbody shipRigidBody;
-    public float topSpeed;
     public float zeroToMaxPerSecond;
-    float acceleration;
     public float forwardVelocity;
+    public int accelerateModeCounter = 0;
+    private float speedVelocity;
+
+    private Vector3 input;
+    private Quaternion targetRotation;
+    private float angle;
+    public float pitchSpeed = 1;
+    public float turnSpeed = 75F;
+    private float yaw;
+
+    public float transitionCooldown = 1F;
+    private float transitionTimer = 0F;
 
     //UPDATES
 
     private void Awake()
     {
         shipRigidBody = GetComponent<Rigidbody>();
-        acceleration = topSpeed / zeroToMaxPerSecond;
         forwardVelocity = 0f;
+        yaw = transform.eulerAngles.y;
     }
 
     private void Update()
     {
-        if(Input.GetKey(KeyCode.W))
+        input.z = Input.GetAxisRaw("Cension");
+        turnCap();
+        calculateDirectionZ();
+        rotation();
+        if (transitionTimer <= 0F)
         {
-            forwardVelocity += acceleration * Time.deltaTime;
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                accelerateModeCounter++;
+                transitionTimer = transitionCooldown;
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                accelerateModeCounter--;
+                transitionTimer = transitionCooldown;
+            }
         }
-        
-        forwardVelocity = Mathf.Min(forwardVelocity, topSpeed);
+        else
+        {
+            transitionTimer -= Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            yaw -= turnSpeed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            yaw += turnSpeed * Time.deltaTime;
+        }
+
+        accelerateModeCounter = Mathf.Clamp(accelerateModeCounter, 0, 3);
+        forwardVelocity = Mathf.SmoothDamp(forwardVelocity, 10 * accelerateModeCounter, ref speedVelocity, zeroToMaxPerSecond);
     }
 
     private void LateUpdate()
@@ -37,4 +74,24 @@ public class MovementControlsShip : MonoBehaviour
     }
 
     //METHODS
+
+    void rotation()
+    {
+        targetRotation = Quaternion.Euler(angle, yaw, 0);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, pitchSpeed * Time.deltaTime);
+    }
+    void calculateDirectionZ()
+    {
+        angle = Mathf.Atan(input.z);
+        angle = Mathf.Rad2Deg * angle;
+    }
+
+    void turnCap()
+    {
+        turnSpeed = 100 - (accelerateModeCounter * 22);
+        if(accelerateModeCounter == 0)
+        {
+            turnSpeed = 25;
+        }
+    }
 }
---
+++ Assets/Test Script/PlayerBattleMode.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBattleMode : MonoBehaviour
+{
+    public Collider[] attackHitBoxes;
+    public int comboPoints = 0;
+    private bool isRunning = false;
+    private bool _weaponHitBox = false;
+    public Animator anim;
+    private float animTime;
+
+
+    /// <summary>
+    /// 1. Program the hitbox collider
+    /// 2. Turn on and off hitbox
+    /// 3. Start attack coroutine
+    /// 4. Input attack sequences
+    /// </summary>
+    /// 
+
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            StartCoroutine("attack");
+        }
+    }
+
+    #region 2. Hitbox Flicker Mechanism
+    void weaponHitBox()
+    {
+        if (_weaponHitBox)
+        {
+            attackHitBoxes[comboPoints].gameObject.GetComponent<Collider>().enabled = true;
+        }
+        else if (!_weaponHitBox)
+        {
+            attackHitBoxes[comboPoints].gameObject.GetComponent<Collider>().enabled = false;
+        }
+    }
+    #endregion
+
+    #region 3. Attack Coroutine
+    IEnumerator attack()
+    {
+        if (!isRunning)
+        {
+            isRunning = true;
+            _weaponHitBox = true;
+            animTrigger();
+            yield return new WaitForSeconds(0.5f);
+            _weaponHitBox = false;
+            Debug.Log("COMBO = " + comboPoints);
+            attackCombo();
+            
+            isRunning = false;
+            //yield return null;
+        }
+    }
+    #endregion
+
+    #region 4. Attack Sequences
+    void attackCombo()
+    {
+        if (comboPoints <= 1)
+        {
+            comboPoints++;
+        }
+        else
+        {
+            comboPoints = 0;
+        }
+
+    }
+    #endregion
+
+    void animTrigger()
+    {
+        if (comboPoints == 0 && isRunning)
+        {
+            anim.SetTrigger("Attack");
+            Debug.Log("0");
+            //anim.["Attack"].normalizedTime = 0.5f;
+        }
+        else if (comboPoints == 1 && isRunning)
+        {
+            anim.SetTrigger("Attack");
+            Debug.Log("1");
+        }
+        else if (comboPoints == 2 && isRunning)
+        {
+            anim.SetTrigger("Attack");
+            Debug.Log("2");
+        }
+
+    }
+
+}
