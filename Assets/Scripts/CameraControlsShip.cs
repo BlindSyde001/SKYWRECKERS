bc28a0a4f5ea9c44de7359ec07986b7c13ce7b55
Ultimo Media <ultimo.media@jmc.edu.au>
2019-06-25 18:19:18 +1000

    Basic Camera Follow

--- Assets/Scripts/CameraControlsShip.cs
+++ Assets/Scripts/CameraControlsShip.cs
@@ -4,15 +4,74 @@ using UnityEngine;
 
 public class CameraControlsShip : MonoBehaviour
 {
+    public GameObject thirdPersonCamera;
+    public GameObject overShoulderCamera;
+
+    AudioListener thirdPersonAudioLis;
+    AudioListener overShoulderListener; 
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        thirdPersonAudioLis = thirdPersonCamera.GetComponent<AudioListener>();
+        overShoulderListener = overShoulderCamera.GetComponent<AudioListener>();
+
+        cameraPositionChange(PlayerPrefs.GetInt("CameraPosition"));
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        switchCamera();
+    }
+
+    public void cameraPositonM()
+    {
+        cameraChangeCounter();
+    }
+
+    void switchCamera()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            cameraChangeCounter();
+        }
+    }
+
+    void cameraChangeCounter()
+    {
+        int cameraPositionCounter = PlayerPrefs.GetInt("CameraPosition");
+        cameraPositionCounter++;
+        cameraPositionChange(cameraPositionCounter);
+    }
+
+    void cameraPositionChange(int camPosition)
+    {
+        if (camPosition > 1)
+        {
+            camPosition = 0;
+        }
+        PlayerPrefs.SetInt("CameraPosition", camPosition);
+
+
+        if (camPosition == 0)
+        {
+            overShoulderCamera.SetActive(true);
+            overShoulderListener.enabled = true;
+
+            thirdPersonAudioLis.enabled = false;
+            thirdPersonCamera.SetActive(false);
+        }
+
+        if (camPosition == 1)
+        {
+            thirdPersonCamera.SetActive(true);
+            thirdPersonAudioLis.enabled = true;
+
+            overShoulderListener.enabled = false;
+            overShoulderCamera.SetActive(false);
+        }
+
     }
 }
