d6bb909264ef3f63af32dae5a72053c9ceee3445
Ben <ultimo.media@jmc.edu.au>
2019-07-02 15:51:31 +1000

    Climbing script
    Added climbing script that lets player climb a trigger zone with this script attached. It is very simple and buggy, but allows for vertical movement.

---
+++ Assets/Scripts/Climbing.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Climbing : MonoBehaviour
+{
+    public GameObject playerCharacter;
+
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+         rb = playerCharacter.GetComponent<Rigidbody>();
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        
+        if(Input.GetKey(KeyCode.L))
+        {
+            
+            rb.useGravity = false;
+            playerCharacter.transform.Translate((Vector3.up *5) * Time.deltaTime, Space.World);
+            Debug.Log("Climbing!");
+        }
+        else
+        {
+            rb.useGravity = true;
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        rb.useGravity = true;
+    }
+}
