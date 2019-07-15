7a7d38222b9fc3141bf534709177aef195d2a7e3
BPowell97 <bennypowell127@gmail.com>
2019-07-15 17:40:07 +1000

    Resource Items + Pick up Items + Text + Inventory

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum InventoryItem { WOOD, FABRIC, METAL }
+public class Inventory : MonoBehaviour
+{
+    public static Inventory Instance;
+
+    //Bools to know whether the player has the resource objects
+    public bool hasWood;
+    public bool hasMetal;
+    public bool hasFabric;
+
+    //Pop up meesgae to get track how many of the resources you have
+    //public string message;
+
+    //Resorce Game Objects
+    public GameObject Wood;
+    public GameObject Fabric;
+    public GameObject Metal;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hasWood = hasMetal = hasFabric = false; //Player has not picked up any items yet
+    }
+
+
+    void CheckInventory() //Pre set up for when the game needs to check for an ship upgrade
+    {
+        Wood.gameObject.SetActive(hasWood);
+        Fabric.gameObject.SetActive(hasFabric);
+        Metal.gameObject.SetActive(hasMetal);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ResourcePickUp.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ResourcePickUp : MonoBehaviour
+{
+    private bool pickUpAllowed; //Bool to allow the player to pick up the gameobject
+
+    public InventoryItem item; //Access to resource gameobjects in inventory script
+    public string message; //Custom message that appears to pick up the resource gameobject
+
+
+    public TextMeshProUGUI woodText;
+    public TextMeshProUGUI fabricText;
+    public TextMeshProUGUI metalText;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Hasn't been set up yet
+        TextMeshProUGUI woodText = GetComponent<TextMeshProUGUI>(); //getting access to the text
+        TextMeshProUGUI fabricText = GetComponent<TextMeshProUGUI>();
+        TextMeshProUGUI metalText = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (pickUpAllowed && Input.GetKeyDown(KeyCode.Q)) //Kept putting me back on the ship with E key
+            PickUp();
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        Debug.Log(collision.name);
+        if (collision.gameObject.name.Equals("Player")) //Text appears when Player is near item, through a collider
+        {
+            UIManager.Instance.ToggleText(message);
+            pickUpAllowed = true; //Item can be picked up
+
+        }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.gameObject.name.Equals("Player")) //Text disappears when Player is not near the item, through a collider
+        {
+            UIManager.Instance.ToggleText("");
+            pickUpAllowed = false;
+        }
+    }
+
+    private void PickUp()
+    {
+        //Inventory.Instance.hasLock = true;
+        if (item == InventoryItem.WOOD) //Item is picked up and appears in inventory
+            Inventory.Instance.hasWood = true;
+        if (item == InventoryItem.FABRIC)
+            Inventory.Instance.hasFabric = true;
+        if (item == InventoryItem.METAL)
+            Inventory.Instance.hasMetal = true;
+        UIManager.Instance.ToggleText("");
+        Destroy(gameObject);
+    }
+}
