103b17fd1443bbc5805e9892211ee83f17e6f263
Samuel Jones <samuel.jones@live.com.au>
2019-08-06 18:41:14 +1000

    Pirate AI more Progress

--- Assets/Scripts/Enemy Scripts/PirateShipAI.cs
+++ Assets/Scripts/Enemy Scripts/PirateShipAI.cs
@@ -13,9 +13,17 @@ public class PirateShipAI : MonoBehaviour
     public float moveSpeed = 7f;
 
     public float lookRadius = 50;
+    private Transform alignPoint;
 
     public Transform playerShipLeft;
     public Transform playerShipRight;
+
+    public List<Transform> bulletEndsRight;
+    public List<Transform> bulletEndsLeft;
+    public Rigidbody bullet;
+    public Transform barrelEnd;
+    public float fireRate = 1f;
+    float nextFire;
     //UPDATES
 
     private void Awake()
@@ -38,7 +46,7 @@ public class PirateShipAI : MonoBehaviour
         //check for which side the ship is on relative to the player and come along side the player on that side
         float leftDistance = Vector3.Distance(transform.position, playerShipLeft.position);
         float rightDistance = Vector3.Distance(transform.position, playerShipRight.position);
-        Transform alignPoint;
+        //Transform alignPoint;
 
         if(leftDistance < rightDistance)
         {
@@ -58,12 +66,43 @@ public class PirateShipAI : MonoBehaviour
         transform.position += transform.forward * moveSpeed * Time.deltaTime;
         } else
         {
-            transform.position = Vector3.Lerp(transform.position, alignPoint.position, 1 * Time.deltaTime);
-            transform.rotation = Quaternion.Slerp(transform.rotation, alignPoint.rotation, 1 * Time.deltaTime);
+            transform.position = Vector3.Lerp(transform.position, alignPoint.position, 1.2f * Time.deltaTime);
+            transform.rotation = Quaternion.Slerp(transform.rotation, alignPoint.rotation, 1.2f * Time.deltaTime);
+            if(Vector3.Distance(transform.position, alignPoint.position) < 2)
+            {
+                ShootingCannons();
+            }
         }
         print(alignPoint);
     }
 
+    #region Cannons
+    void ShootingCannons()
+    {
+        if (Time.time > nextFire && alignPoint == playerShipLeft)
+        {
+
+            nextFire = Time.time + fireRate;
+            Rigidbody bulletInstance;
+            foreach (Transform x in bulletEndsRight)
+            {
+                bulletInstance = Instantiate(bullet, x.position, x.rotation) as Rigidbody;
+                bulletInstance.AddForce(barrelEnd.forward * 4500);
+            }
+        }
+        else if (Time.time > nextFire && alignPoint == playerShipRight)
+        {
+
+            nextFire = Time.time + fireRate;
+            Rigidbody bulletInstance;
+            foreach (Transform y in bulletEndsLeft)
+            {
+                bulletInstance = Instantiate(bullet, y.position, y.rotation) as Rigidbody;
+                bulletInstance.AddForce(barrelEnd.forward * -4500);
+            }
+        }
+    }
+    #endregion
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
