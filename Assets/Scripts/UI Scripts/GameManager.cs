6d9b81be806bc8aedcc615efa86db48c75a6b1ca
BlindSyde001 <samuel.jones@live.com.au>
2019-09-30 12:00:02 +1000

    checkpoint start

--- Assets/Scripts/UI Scripts/GameManager.cs
+++ Assets/Scripts/UI Scripts/GameManager.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //VARIABLES
+    private static GameManager instance;
+    public Vector3 lastCheckpointPos;
+
+    //UPDATES
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        }
+        else {
+            Destroy(gameObject); }
+    }
     void Start()
     {
-        Cursor.visible = false; //Turing off cursor so it doesn't annoy players operating the ship or moving
+        Cursor.visible = false;
     }
-
-    // Update is called once per frame
     void Update()
     {
         
--- Assets/Scripts/UI Scripts/LOADSCENE.cs
+++ Assets/Scripts/UI Scripts/LOADSCENE.cs
@@ -5,18 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class LOADSCENE : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    //METHODS
     public void LoadGame()
     {
         SceneManager.LoadScene(1);
