7993e72b44e775ffde61cea9ed905f0513c43a99
Samuel Jones <samuel.jones@live.com.au>
2019-10-23 15:31:04 +1100

    testing stuff

---
+++ Assets/Scripts/World Scripts/MovingPlatforms.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatforms : MonoBehaviour
+{
+    //UPDATES
+    private void Update()
+    {
+        transform.position = transform.position + transform.right * Mathf.Sin(Time.time * 2) * 1/10;
+    }
+}
